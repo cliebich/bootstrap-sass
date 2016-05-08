d599ecf6789eae1d8636aba1afc1c5a8884dccfe
cliebich <chris@christhomasdesign.com>
2016-05-07 21:59:01 -0700

    added hash and localstorage js

--- JavaScript.js
+++ JavaScript.js
@@ -1,56 +1,73 @@
-﻿// when tab is fired change appearance
-$('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+﻿$(document).ready(function () {
+    // show active tab on reload
+    if (location.hash !== '') $('a[href="' + location.hash + '"]').tab('show');
+    else {
+        $('a[href="#family"]').tab('show'); //  fix for jump to tab anchor by setting default tab
+    } 
+
+    // remember the hash in the URL without jumping
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        if (history.pushState) {
+            history.pushState(null, null, '#' + $(e.target).attr('href').substr(1));
+            var tab = window.location.hash.substr(1);
+            var head = $("#head");
+
+            $(".goToLink").removeClass('disabled');
+            $('.goToLink[data-tabname = ' + tab + ']').addClass('disabled');
+            if (tab == "family") {
+                head.addClass("mcguff-main");
+            } if (tab == "company") {
+                head.addClass("mcguff-company");
+            }
+            if (tab == "compounding") {
+                head.addClass("mcguff-compounding");
+            }
+        } else {
+            location.hash = '#' + $(e.target).attr('href').substr(1);
+        }
+    });
+});
+
+// when tab is fired change appearance
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
     var tab = $(e.target);
-    var allTabs = $('a[data-toggle="tab"]');
-    allTabs.removeClass("disabled");
-    tab.addClass("disabled");
+
+    //tab.addClass("disabled");
     var head = $("#head");
-    var navbar = $(".navbar");
-    var comLogo = $(".company-logo");
+    //var navbar = $(".navbar");
+    //var comLogo = $(".company-logo");
 
     head.removeClass("mcguff-main mcguff-noanim mcguff-company mcguff-compounding");
-    navbar.removeClass("navbar-family navbar-company navbar-compounding");
-    comLogo.removeClass("logo-family logo-company logo-compounding");
+    //navbar.removeClass("navbar-family navbar-company navbar-compounding");
+    //comLogo.removeClass("logo-family logo-company logo-compounding");
 
     if (tab.attr("id") == "family") {
         head.addClass("mcguff-main");
-        navbar.addClass("navbar-family");
-        comLogo.addClass("logo-family");
+        //navbar.addClass("navbar-family");
+        //comLogo.addClass("logo-family");
     } else if (tab.attr("id") == "company") {
-        head.addClass("mcguff-company")
-        navbar.addClass("navbar-company");
-        comLogo.addClass("logo-company");
-    } else if (tab.attr("id") == "Compounding") {
+        head.addClass("mcguff-company");
+        //navbar.addClass("navbar-company");
+        //comLogo.addClass("logo-company");
+    } else if (tab.attr("id") == "compounding") {
         head.addClass("mcguff-compounding");
-        navbar.addClass("navbar-compounding");
-        comLogo.addClass("logo-compounding");
+        //navbar.addClass("navbar-compounding");
+        //comLogo.addClass("logo-compounding");
     }
-    // set localstorage tabname based on id of tab
-    window.localStorage.LastActiveTabName = tab.attr("id");
 });
 
-// when click on nav set localstorage tabname then goto homepage if necessary
-// otherwise just show tab based on data-tabname
+
 $(".goToLink").click(function () {
     var TabName = $(this).data("tabname");
     if ($(".homepage-flag").length == 0) {
-        window.localStorage.LastActiveTabName = TabName;
         window.location.href = "/";
     } else {
-        if ($('.navbar-collapse').hasClass("navbar-collapse in")) {
+        if ($('.navbar-collapse').hasClass("navbar-collapse collapse in")) { // remove second collapse for mcguff
             $('.navbar-collapse').collapse('hide');
         }
-        $("#" + TabName).tab("show");
+        $("#myTab a[href=#" + TabName + "]").tab('show');
     }
 });
-
-// show correct tab when page loads based on localstorage
-$(document).ready(function () {
-    if (window.localStorage.LastActiveTabName != null && window.localStorage.LastActiveTabName !== "") {
-        $("#" + window.localStorage.LastActiveTabName).tab('show');
-    }
-});
-
 
 // use these links for nav
 //<a href="#" class="goToLink" data-tabname="family">Home</a>
